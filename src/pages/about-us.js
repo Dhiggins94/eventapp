192ec1c85193ba701ed10d22802d55e5499beae7
Divine Higgins <dhiggins94@gmail.com>
2023-01-25 01:10:11 -0500

    setting up pages in event folder

---
+++ src/pages/about-us.js
@@ -0,0 +1,13 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>About US Page</h1>
+      <p>
+        Charizard Hippowdon Dome Fossil in a world we must defend Toxicroak
+        Latias Mewtwo. Fuchsia City Hitmontop Pidgeotto Throh Throh Cherubi
+        Mesprit. Dig Gastrodon Duskull Pokemon Skuntank Dialga Volcano Badge.
+      </p>
+    </div>
+  );
+};
+export default Page;
---
+++ src/pages/events/index.js
@@ -0,0 +1,22 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events page</h1>
+      <div>
+        <a href="">
+          <img />
+          <h2>Events in London</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2>Events in San fransico</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2>Events in New York</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+export default Page;
