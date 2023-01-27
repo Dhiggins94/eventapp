15a21ad785f63f7f909ab94ac48d8a3c10f26ca7
Divine Higgins <dhiggins94@gmail.com>
2023-01-27 14:23:47 -0500

    connected data to our all events page

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,23 +1,18 @@
 /** @jsxImportSource theme-ui */
+import Image from "next/image";
 import { jsx, ThemeProvider } from "theme-ui";
 
-const EventsPage = () => {
+const EventsPage = ({ data }) => {
   return (
     <div>
       <h1 sx={{ color: "primary" }}>Event Page</h1>
       <div>
-        <a href="">
-          <img />
-          <h2>Events in London</h2>
-        </a>
-        <a href="">
-          <img />
-          <h2>Events in San fransico</h2>
-        </a>
-        <a href="">
-          <img />
-          <h2>Events in New York</h2>
-        </a>
+        {data.map((ev) => (
+          <a key={ev.id} href={`/events/${ev.id}`}>
+            <Image src={ev.image} alt={ev.title} width={100} height={100} />
+            <h2>{ev.title}</h2>
+          </a>
+        ))}
       </div>
     </div>
   );
@@ -25,14 +20,12 @@ const EventsPage = () => {
 export default EventsPage;
 // all events aka dyanmic pages
 
-
 export async function getStaticProps() {
-  const {events_categories} = import('/data/data.json')
+  const { events_categories } = await import('data/data.json');
   return {
     props: {
-    data: events_categories
-  }
+      data: events_categories,
+    },
+  };
 }
-
-}
-//static generating our pages 
+//static generating our pages
