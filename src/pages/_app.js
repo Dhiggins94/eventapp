2b9621040a93bcb866f630216b7cef470b5fbef1
Divine Higgins <dhiggins94@gmail.com>
2023-03-02 20:28:17 -0500

    finished data fetching

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource theme-ui */
 // theme ui
-// import "@/styles/globals.css";
+import "@/styles/globals.css";
 import { jsx, ThemeProvider } from "theme-ui";
 import tailwindStyle from "theme";
 export default function App({ Component, pageProps }) {
--- src/pages/index.js
+++ src/pages/index.js
@@ -38,9 +38,11 @@ export default function Home({ data }) {
         <main className={styles.main}>
           {data.map((ev) => (
             <Link key={ev.id} href={`/events/${ev.id}`} passHref>
-              <Image width={300} height={300} alt={ev.title} src={ev.image} />
-              <h2>{ev.title}</h2>
-              <p>{ev.description}</p>
+             
+                <Image width={300} height={300} alt={ev.title} src={ev.image} />
+                <h2>{ev.title}</h2>
+                <p>{ev.description}</p>
+            
             </Link>
           ))}
         </main>
